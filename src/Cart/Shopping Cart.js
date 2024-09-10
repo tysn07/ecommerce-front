a739cc614c70b2ac85097f749fd26face16ab056
tysn07 <tyler0702@naver.com>
2024-09-10 23:36:54 +0900

    :recycle: refactor address dropmenu

--- src/Cart/Shopping Cart.js
+++ src/Cart/Shopping Cart.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 import Navbar from "../Navbar";
-import AddressDropmenu from "./AddressDropmenu";
 import {useState,useEffect} from "react";
 
 function ShoppingCart(){
-
     axios.defaults.withCredentials = true;
     const basket = new Map;
     const displayBasket = new Map;
-    const address = 1;
     const length = localStorage.length;
+
+    const [address,setaddress] = useState([]);
+    const [select,setselect] = useState([]);
 
     for (let i = 0; i < length; i++) {
         const prepkey = localStorage.key(i);
@@ -19,8 +19,6 @@ function ShoppingCart(){
         const quantity = localStorage.getItem(prepkey);
               basket.set(productId, quantity);
               displayBasket.set(productName, quantity)
-
-
     }
 
     const arr = Array.from(displayBasket).map(([productName,quantity])=>([productName,quantity]))
@@ -38,19 +36,59 @@ function ShoppingCart(){
         window.location.reload();
     }
 
+    useEffect(()=>{
+        axios.get("https://back.son7shop.com/users/address/list")
+            .then(response => {
+                setaddress(response.data)
+                console.log(response.data);
+            });
 
+    }, []);
 
+    function myFunction() {
+        document.getElementById("myDropdown").classList.toggle("show");
+    }
+    const addressSelect = (a) =>{
+        setselect(a);
+    }
+
+    window.onclick = function(event) {
+        if (!event.target.matches('.dropbtn')) {
+            var dropdowns = document.getElementsByClassName("dropdown-content");
+            var i;
+            for (i = 0; i < dropdowns.length; i++) {
+                var openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show')) {
+                    openDropdown.classList.remove('show');
+                }
+            }
+        }
+    }
 
     return(
         <>
            <Navbar/>
             <ul>
                 {arr.map((arr, index) => <li className="basket" key={index}>상품:&nbsp;{arr.at(0)}&emsp;&emsp;&emsp;&emsp;수량:&nbsp;{arr.at(1)}개</li>)}
-                <AddressDropmenu/>
-                <button className="paybutton" onClick={pushItem}>결제하기</button>
+                <div className="aligncart">
+                    <div className="dropdown">
+                        <button onClick={myFunction} className="dropbtn">주소</button>
+                        <div id="myDropdown" className="dropdown-content">
+                            {address.map((item, index) => (
+                                // Render a div for each item in the array
+                                <div onClick={() => addressSelect(item)} key={index}>
+                                    {item}
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                    <h>{select}</h>
+                </div>
+                <button className="paybutton" onClick={pushItem}>주문하기</button>
             </ul>
 
         </>
     )
 }
+
 export default ShoppingCart
